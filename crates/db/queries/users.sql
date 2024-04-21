8c76c1bc5715dd5813cf0e39eb13d81b415db91a
Josh H <josh@hansonslogic.com>
2024-04-21 07:54:27 +0000

    Forms - Front/Back End Validation

--- crates/db/queries/users.sql
+++ crates/db/queries/users.sql
@@ -9,3 +9,10 @@ FROM users;
 -- Cornucopia will use the above definition to generate a Rust function called 
 -- get_users to access the database. Note cornucopia checks the query at code 
 -- generation time against Postgres.
+
+-- 👇 add `create_user` query
+--! create_user
+INSERT INTO users (email)
+VALUES(:email);
+-- INSERT INTO users (email, hashed_password)
+-- VALUES(:email, :hashed_password);
